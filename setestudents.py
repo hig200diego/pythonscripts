f599346bab40523a3a305dd8db862b8f45e9f1bf
Diego Freire <hig200diego@gmail.com>
2019-02-11 14:29:02 -0200

    script sobre operacoes matematicas

---
+++ setestudents.py
@@ -0,0 +1,9 @@
+math_students = {"Matthew", "Helen", "Prashant", "James", "Arpana"}
+biology_students = {"Jane", "Matthew", "Charlotte", "Mesut", "Oliver", "James"}
+
+#set union
+united_classes = math_students | biology_students
+print(united_classes)
+ 
+intersection_classes = math_students & biology_students
+print(intersection_classes)
